4eead90e34d86c97731448f3f8e090aa5322de91
bragagustavo <gustavo.b.moraes2@gmail.com>
2025-03-28 23:47:11 -0300

    first commit

---
+++ src/main/java/com/bragadev/SmartCookAI/SmartCookAiApplication.java
@@ -0,0 +1,13 @@
+package com.bragadev.SmartCookAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCookAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCookAiApplication.class, args);
+	}
+
+}
