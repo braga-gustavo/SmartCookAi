f6c817b75bea80cee0bbeb74453a8a8e45d4a70a
bragagustavo <gustavo.b.moraes2@gmail.com>
2025-04-01 10:26:33 -0300

    entity creation

--- src/main/java/com/bragadev/SmartCookAI/model/Ingredient.java
+++ src/main/java/com/bragadev/SmartCookAI/model/Ingredient.java
@@ -1,4 +1,53 @@
 package com.bragadev.SmartCookAI.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ingredient")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
+
+    private static final Long id = 1L;
+    private String name;
+    private String category;
+    private Integer quantity;
+    private LocalDate expirationDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
