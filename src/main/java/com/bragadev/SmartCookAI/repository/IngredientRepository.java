399068693b59b5ebdaa60f6ca7e6fae32e808d9c
bragagustavo <gustavo.b.moraes2@gmail.com>
2025-04-01 10:26:13 -0300

    repository correction

--- src/main/java/com/bragadev/SmartCookAI/repository/IngredientRepository.java
+++ src/main/java/com/bragadev/SmartCookAI/repository/IngredientRepository.java
@@ -2,7 +2,9 @@ package com.bragadev.SmartCookAI.repository;
 
 import com.bragadev.SmartCookAI.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IngredientRepository extends JpaRepository <Ingredient, Long> {
 }
 
