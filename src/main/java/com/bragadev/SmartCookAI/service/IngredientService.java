1f6e17668d43f353ed1cbf70d7a63020ef045086
bragagustavo <gustavo.b.moraes2@gmail.com>
2025-04-02 11:11:10 -0300

    ingredient service implementation

--- src/main/java/com/bragadev/SmartCookAI/service/IngredientService.java
+++ src/main/java/com/bragadev/SmartCookAI/service/IngredientService.java
@@ -1,4 +1,36 @@
 package com.bragadev.SmartCookAI.service;
 
+import com.bragadev.SmartCookAI.model.Ingredient;
+import com.bragadev.SmartCookAI.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class IngredientService {
+
+    @Autowired
+    IngredientRepository repository;
+
+    public Ingredient save(Ingredient ingredient) {
+        return repository.save(ingredient);
+    }
+
+    public List<Ingredient> listIngredients() {
+        return repository.findAll();
+    }
+
+    public Optional<Ingredient> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Ingredient update(Ingredient ingredient) {
+        return repository.save(ingredient);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
